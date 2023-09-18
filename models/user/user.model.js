ee812f217d023db70a27d4a39e0741910c9cafa6
Anton Latanskyy <52884811+kalonium16@users.noreply.github.com>
2023-09-18 13:41:07 +0300

    Update user.model.js

--- models/user/user.model.js
+++ models/user/user.model.js
@@ -2,17 +2,15 @@ const userData = require('./user.mongo');
 const { getUserTotalSpend, getUserTotalRefund } = require('../transaction/transaction.model')
 const ITEMS_PER_PAGE = 10;
 
-async function getUsers(page, amount) {
+async function getUsers(page=1, amount) {
     let itemsOnPage = amount ? amount : ITEMS_PER_PAGE;
     const options = {
         page: page,
         limit: itemsOnPage,
       };
     let users = await userData.paginate({},options);
-    let response = [];
-    response = await setUpResponseData(itemsOnPage, users.docs);
-    return response;
-
+    users.docs = await setUpResponseData(itemsOnPage, users.docs);
+    return users;
 }
 
 async function setUpResponseData(itemsOnPage, users) {
